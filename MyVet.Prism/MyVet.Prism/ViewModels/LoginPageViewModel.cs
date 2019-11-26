c4a544548157815ff15e6839122b074723905246
Daniel Lemus Zuluaga <chani55510@hotmail.com>
2019-11-26 16:14:08 -0500

    Login With Service

--- MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Commands;
+﻿using MyVet.Common.Models;
+using MyVet.Common.Services;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
@@ -9,13 +11,17 @@ namespace MyVet.Prism.ViewModels
 {
     public class LoginPageViewModel : ViewModelBase
     {
+        private readonly IApiService _apiService;
         private string _password;
         private bool _isRunning;
         private bool _isEnable;
         private DelegateCommand _loginCommand;
 
-        public LoginPageViewModel(INavigationService navigationService) : base (navigationService)
+        public LoginPageViewModel(
+            INavigationService navigationService,
+            IApiService apiService) : base (navigationService)
         {
+            _apiService = apiService;
             Title = "Login";
             IsEnable = true;
         }
@@ -56,6 +62,28 @@ namespace MyVet.Prism.ViewModels
                 return;
             }
 
+            IsRunning = true;
+            IsEnable = false;
+
+
+            var request = new TokenRequest
+            {
+                Password = Password,
+                Username = Email
+            };
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
+
+            IsRunning = false;
+            IsEnable = true;
+
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Email or password incorrect.", "Accept");
+                Password = string.Empty;
+                return;
+            }
             await App.Current.MainPage.DisplayAlert("Ok", " /(°-°)/ ", "Accept");
         }
 
